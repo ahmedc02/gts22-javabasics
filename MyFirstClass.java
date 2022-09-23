5c2a21408d972f8268da4a8d744556675daed551
Ahmed Al Sahli <12103658@edu.campus02.at>
2022-09-23 17:10:18 +0200

    My First Class created; args as input parametre written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    // class names should be written in CamelCase and starts with Upper Case (Test)
+
+    //psvm
+    public static void main(String[] args) {
+        // erste Ausgabe
+        System.out.println("HAHAHA, KAK");
+
+        // args
+        // sout + tab
+        System.out.println(Arrays.toString(args)); // toString() gives all Eigenschaften die in the class
+        // MyFirstClass -> Edit Configurations
+    }
+}
